6bc480b6ae57bf03c2b8309849dd955d489c9fcd
lolverae <alberto.olvera.espinosa@gmail.com>
2024-01-03 20:48:47 -0600

    Initial commit

---
+++ setup-debian-wsl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Update the package list and upgrade installed packages
+sudo apt update
+sudo apt upgrade -y
+
+# Install essential tools and utilities
+sudo apt install -y git python3-pip exa unzip bat gh zsh curl
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt install -y nodejs
+
+# Set Zsh as the default shell and install oh-my-zsh with powerlevel10k theme
+sudo chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Neovim
+wget -O linux-amd64.tar.gz https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz
+tar xzvf nvim-linux64.tar.gz
+
+# Install RipGrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+# Install Kubernetes related items
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo brew install derailed/k9s/k9s
+
+# Install Golang
+wget -O go1.21.5.linux-amd64.tar.gz https://go.dev/dl/go1.21.5.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.21.5.linux-amd64.tar.gz
+
+# Clean up
+sudo apt autoremove -y
+sudo apt clean
+rm ripgrep_13.0.0_amd64.deb
+
+echo "Setup completed successfully!"
+
