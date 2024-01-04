d5a79d31f76c7e685a36be7496fd4396283f7680
lolverae <alberto.olvera.espinosa@gmail.com>
2024-01-03 21:36:29 -0600

    Add nvchad config

---
+++ nvchad-config/chadrc.lua
@@ -0,0 +1,25 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "oceanic-light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+
+  nvdash = {
+    load_on_startup = true,
+  },
+  transparency = true
+}
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
---
+++ nvchad-config/plugins.lua
@@ -0,0 +1,86 @@
+---@type NvPluginSpec[]
+local plugins = {
+
+  -- Override plugin definition options
+
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      -- format & linting
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require "custom.configs.null-ls"
+        end,
+      },
+    },
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        -- lua stuff
+        "lua-language-server",
+        "stylua",
+
+        -- web dev stuff
+        "css-lsp",
+        "html-lsp",
+        "gopls",
+      },
+    }
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+      },
+      indent = {
+        enable = true,
+      },
+
+    }
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = {
+        enable = true,
+      },
+
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+          },
+        },
+      },
+    }
+  },
+
+  -- Install a plugin
+  {
+    "max397574/better-escape.nvim",
+    event = "InsertEnter",
+    config = function()
+      require("better_escape").setup()
+    end,
+  },
+
+}
+
+return plugins
