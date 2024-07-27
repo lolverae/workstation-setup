94bffa30bf7780ed2ccc28dbe21683c46c7f3eb5
lolverae <alberto.olvera.espinosa@gmail.com>
2024-07-27 00:03:36 -0600

    Add extra formatters

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,23 @@
 local options = {
 
-
   formatters_by_ft = {
     lua = { "stylua" },
+
+    -- Web stuff
     css = { "prettier" },
     html = { "prettier" },
-    markdown = { "prettier" },
-    
-    go = {"gofumpt"},
-  },
+    markdown = { "deno_fmt" },
+    javascript = { "deno_fmt" },
+    typescript = { "deno_fmt" },
 
+    -- Devops
+    go = { "gofumpt" },
+    python = { "ruff_format" },
+    terraform = { "terraform_fmt" },
+    json = { "yq" },
+    yaml = { "yq" },
+    bash = { "beautysh" },
+  },
   format_on_save = {
     lsp_fallback = true,
   },
